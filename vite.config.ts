b146fa4bf9de8aaebcbbbb8eed4cdb90762feea5
Timluk1 <main345242@gmail.com>
2025-03-26 10:37:13 +0300

    Images optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import tailwindcss from "@tailwindcss/vite";
 import autoprefixer from "autoprefixer";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        ViteImageOptimizer({
+            png: {
+                quality: 80,
+                compressionLevel: 8,
+            },
+            jpeg: {
+                quality: 85,
+                progressive: true,
+            },
+            webp: {
+                quality: 85,
+            },
+            svg: {
+                multipass: true,
+                js2svg: {
+                    pretty: true,
+                    indent: 2,
+                },
+            },
+        }),
+    ],
     base: "/Personal-portfolio/",
     css: {
         postcss: {
